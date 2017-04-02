a24bca8deec0f211f77b9a4182f7af3a977bf27b
chowyong_choi <c494003007@qq.com>
2017-04-02 16:18:39 +0800

    amend the attribute "describe" to "describeself" of NormalUserResume.java

--- src/main/java/com/recordme/modules/usermanage/entity/NomalUserResume.java
+++ src/main/java/com/recordme/modules/usermanage/entity/NomalUserResume.java
@@ -26,7 +26,7 @@ public class NomalUserResume implements Serializable{
     //奖项
     public String prize;
     //自我描述
-    public String describe;
+    public String describeself;
     //爱好
     public String hobby;
     //期望工作地点
@@ -49,4 +49,152 @@ public class NomalUserResume implements Serializable{
     public Date graduateTime;
     //是否兼职
     public int partTimeJob;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWorkedCompany() {
+        return workedCompany;
+    }
+
+    public void setWorkedCompany(String workedCompany) {
+        this.workedCompany = workedCompany;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getWorkExperience() {
+        return workExperience;
+    }
+
+    public void setWorkExperience(int workExperience) {
+        this.workExperience = workExperience;
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+
+    public void setPrize(String prize) {
+        this.prize = prize;
+    }
+
+    public String getDescribeself() {
+        return describeself;
+    }
+
+    public void setDescribeself(String describeself) {
+        this.describeself = describeself;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getHopeWorkingAddress() {
+        return hopeWorkingAddress;
+    }
+
+    public void setHopeWorkingAddress(String hopeWorkingAddress) {
+        this.hopeWorkingAddress = hopeWorkingAddress;
+    }
+
+    public String getHopeYearlySalary() {
+        return hopeYearlySalary;
+    }
+
+    public void setHopeYearlySalary(String hopeYearlySalary) {
+        this.hopeYearlySalary = hopeYearlySalary;
+    }
+
+    public String getNowWorkingAddress() {
+        return nowWorkingAddress;
+    }
+
+    public void setNowWorkingAddress(String nowWorkingAddress) {
+        this.nowWorkingAddress = nowWorkingAddress;
+    }
+
+    public String getNowYearlySalary() {
+        return nowYearlySalary;
+    }
+
+    public void setNowYearlySalary(String nowYearlySalary) {
+        this.nowYearlySalary = nowYearlySalary;
+    }
+
+    public String getWorkDuty() {
+        return workDuty;
+    }
+
+    public void setWorkDuty(String workDuty) {
+        this.workDuty = workDuty;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public Date getStartUnivercityTime() {
+        return startUnivercityTime;
+    }
+
+    public void setStartUnivercityTime(Date startUnivercityTime) {
+        this.startUnivercityTime = startUnivercityTime;
+    }
+
+    public Date getGraduateTime() {
+        return graduateTime;
+    }
+
+    public void setGraduateTime(Date graduateTime) {
+        this.graduateTime = graduateTime;
+    }
+
+    public int getPartTimeJob() {
+        return partTimeJob;
+    }
+
+    public void setPartTimeJob(int partTimeJob) {
+        this.partTimeJob = partTimeJob;
+    }
 }
