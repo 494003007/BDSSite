440cebbe1ba78f9d1f11574b13e032cb8e0da4c9
hzd <hzd138@qq.com>
2017-04-07 17:15:09 +0800

    修改用户注册

--- src/main/java/com/recordme/modules/usermanage/controller/AccountManageController.java
+++ src/main/java/com/recordme/modules/usermanage/controller/AccountManageController.java
@@ -11,6 +11,7 @@ import org.apache.shiro.crypto.hash.Md5Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,8 +35,8 @@ public class AccountManageController {
     @Autowired
     private RoleService roleService;
     @RequestMapping(value = "register",method = RequestMethod.GET)
-    public String registerPage(@ModelAttribute UserInfo user){
-
+    public String registerPage(Model model, @ModelAttribute UserInfo user){
+        model.addAttribute("userInfo",new UserInfo());
         return "/usermanage/register";
     }
     @RequestMapping(value = "register",method = RequestMethod.POST)
