7894ef9895303ab6b666193991021df1439a8405
hzd <hzd138@qq.com>
2017-05-08 11:47:01 +0800

    添加注解

--- src/main/java/com/recordme/modules/usermanage/dao/LexiconCategoriesDao.java
+++ src/main/java/com/recordme/modules/usermanage/dao/LexiconCategoriesDao.java
@@ -3,9 +3,11 @@ package com.recordme.modules.usermanage.dao;
 import com.recordme.modules.usermanage.entity.LexiconCategories;
 import com.recordme.modules.usermanage.entity.SysPermission;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Ed_cc on 2017/5/6.
  */
+@Repository
 public interface LexiconCategoriesDao extends CrudRepository<LexiconCategories, Long> {
 }
--- src/main/java/com/recordme/modules/usermanage/services/LexiconCategoriesService.java
+++ src/main/java/com/recordme/modules/usermanage/services/LexiconCategoriesService.java
@@ -5,10 +5,12 @@ import com.recordme.modules.usermanage.dao.LexiconCategoriesDao;
 import com.recordme.modules.usermanage.dao.PermissionDao;
 import com.recordme.modules.usermanage.entity.LexiconCategories;
 import com.recordme.modules.usermanage.entity.SysPermission;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Ed_cc on 2017/5/6.
  */
+@Component
 public class LexiconCategoriesService extends BaseService<LexiconCategoriesDao, LexiconCategories, Long> {
 
 }
