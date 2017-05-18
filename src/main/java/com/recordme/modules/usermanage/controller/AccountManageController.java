5a3853b14551248227d206a2a0f355ad67ed218a
hzd <hzd138@qq.com>
2017-05-18 18:31:39 +0800

    login

--- src/main/java/com/recordme/modules/usermanage/controller/AccountManageController.java
+++ src/main/java/com/recordme/modules/usermanage/controller/AccountManageController.java
@@ -4,10 +4,13 @@ package com.recordme.modules.usermanage.controller;
 import com.recordme.modules.usermanage.entity.UserInfo;
 import com.recordme.modules.usermanage.services.RoleService;
 import com.recordme.modules.usermanage.services.UserService;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import sun.misc.BASE64Encoder;
+
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
@@ -53,31 +57,35 @@ public class AccountManageController {
     }
 
 
-    @RequestMapping(value="login",method= RequestMethod.POST)
-    public String login(HttpServletRequest request, Map<String, Object> map) throws Exception {
+        @RequestMapping(value="loginI",method= RequestMethod.POST)
+    public String login(String username,String password,Boolean rememberMe,HttpServletRequest request, Map<String, Object> map) throws Exception {
+        Subject subject = SecurityUtils.getSubject();
 
-        System.out.println("HomeController.login()");
-        // 登录失败从request中获取shiro处理的异常信息。
-        // shiroLoginFailure:就是shiro异常类的全类名.
-        String exception = (String) request.getAttribute("shiroLoginFailure");
-
-        System.out.println("exception=" + exception);
-        String msg = "";
-        if (exception != null) {
-            if (UnknownAccountException.class.getName().equals(exception)) {
-                System.out.println("UnknownAccountException -- > 账号不存在：");
-                msg = "UnknownAccountException -- > 账号不存在：";
-            } else if (IncorrectCredentialsException.class.getName().equals(exception)) {
-                System.out.println("IncorrectCredentialsException -- > 密码不正确：");
-                msg = "IncorrectCredentialsException -- > 密码不正确：";
-            } else if ("kaptchaValidateFailed".equals(exception)) {
-                System.out.println("kaptchaValidateFailed -- > 验证码错误");
-                msg = "kaptchaValidateFailed -- > 验证码错误";
-            } else {
-                msg = "else >> "+exception;
-                System.out.println("else -- >" + exception);
-            }
+        UsernamePasswordToken upt;
+        if(rememberMe != null && rememberMe){
+            upt = new UsernamePasswordToken(username,password,rememberMe);
+        }else{
+            upt = new UsernamePasswordToken(username,password);
         }
+
+        String msg = "";
+
+        try{
+            subject.login(upt);
+            return "redirect:index";
+        }catch (IncorrectCredentialsException e){
+            System.out.println("IncorrectCredentialsException -- > 密码不正确：");
+            msg = "IncorrectCredentialsException -- > 密码不正确：";
+        }catch (UnknownAccountException e){
+            System.out.println("UnknownAccountException -- > 账号不存在：");
+            msg = "UnknownAccountException -- > 账号不存在：";
+        }catch (Exception e){
+            msg = "else >> "+e;
+            System.out.println("else -- >" + e);
+        }
+
+
+
         map.put("msg", msg);
         // 此方法不处理登录成功,由shiro进行处理.
         return "usermanage/login";
