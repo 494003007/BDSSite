b026aeee0d57f982afb0267445c8b248b2adff74
hzd <hzd138@qq.com>
2017-08-31 12:03:20 +0800

    1.将ListDto支持类型改为Collection 2.去除使用googleApi的文字加载，提升页面响应速度 3.为性能监控增加错误页

--- src/main/java/com/bdssite/modules/common/dto/ListDto.java
+++ src/main/java/com/bdssite/modules/common/dto/ListDto.java
@@ -2,20 +2,21 @@ package com.bdssite.modules.common.dto;
 
 import com.bdssite.modules.common.RequestStatus;
 
+import java.util.Collection;
 import java.util.List;
 
 public class ListDto<S> extends BaseDto {
-    private List<S> entityList;
-    public ListDto(RequestStatus status, List<S> entityList) {
+    private Collection<S> entityList;
+    public ListDto(RequestStatus status, Collection<S> entityList) {
         super(status);
         this.entityList = entityList;
     }
 
-    public List<S> getEntityList() {
+    public Collection<S> getEntityList() {
         return entityList;
     }
 
-    public void setEntityList(List<S> entityList) {
+    public void setEntityList(Collection<S> entityList) {
         this.entityList = entityList;
     }
 }
