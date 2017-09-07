1e7bd34e82d3c6e5dbba61d8ddffd7b377e088d0
kebencool <peng522642110@icloud.com>
2017-09-07 15:20:38 +0800

    1.更改導航欄初始化

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
         st_sb_scroll: 0,
         st_sb_right: 0,
         st_sb_custom: 0,
-        st_sb_toggled: 1,
+        st_sb_toggled: 0,
         st_layout_boxed: 0
     };
     /* End Default settings */
