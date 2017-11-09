5e73db2469a7321c1b679a211485c5aa23cc1a2e
kebencool <peng522642110@icloud.com>
2017-11-09 16:57:14 +0800

    1. 上传头像 2.修改（sideBar中） 易职搜 图标url bug 3.修改（sideBar中） 头像 url 4.增加文件上传 GlobalExceptionHandlerConfig.java 5.增加 application 配置文件文件上传大小限制

--- src/main/java/com/bdssite/BDSSiteApplication.java
+++ src/main/java/com/bdssite/BDSSiteApplication.java
@@ -4,6 +4,9 @@ import com.bdssite.modules.common.repository.ExtJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -21,6 +24,6 @@ public class BDSSiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BDSSiteApplication.class, args);
 	}
-
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
 
 }
