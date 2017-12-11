1670ebd3b02760e9e656a06f8558603baa056e15
Joey_Tsai <c494003007@qq.com>
2017-12-11 12:25:56 +0800

    1.echart城市图

---
+++ src/main/java/com/bdssite/modules/echart/controller/EchartController.java
@@ -0,0 +1,24 @@
+package com.bdssite.modules.echart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by Joey_Tsai on 2017/12/11.
+ */
+@Controller
+@RequestMapping("echart")
+public class EchartController {
+    @RequestMapping(value = "page",method = RequestMethod.GET)
+    public String echartPage(@RequestParam Map<String, Object> params, HttpServletResponse response) throws Exception{
+
+        return "/echartPage/cityMapPage";
+
+    }
+}
